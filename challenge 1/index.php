d5308d3865efeb51554f58cddf99b179e70e46c1
abdelhak20azur02@gmail.com <abdelhak20azur02@gmail.com>
2023-12-07 02:33:08 +0100

    creating a pagephp with variables and use it in a echo

---
+++ challenge 1/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>challenge 1</title>
+</head>
+<body>
+    <h1>Welcome to PHP</h1>
+
+    <?php
+    $greeting = 'Hello';
+    $toWho = 'World';
+    $name = 'ABD-ELHAQ AZROUR';
+    $domain = 'Tech';
+
+    echo $greeting . " " . $toWho . " . I'm a developer with a really great patience into " . $domain
+    ?>
+</body>
+</html>
