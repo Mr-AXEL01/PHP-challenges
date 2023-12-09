be3fd44ffc338c59d9822be211bb0c740e81d95a
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2023-12-09 17:42:17 +0100

    start challenge 5 and starting with creating an array to stock information

---
+++ challenge 5/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome to PHP</h1>
+    <?php
+    $movies=[
+        [
+            'title'=>'Ali Zaoua',
+            'release_year'=>'2000'
+        ],
+        [
+            'title'=>'Marock',
+            'release_year'=>'2005'
+        ],
+        [
+            'title'=>'Trances',
+            'release_year'=>'1981'
+        ],
+        [
+            'title'=>'Horses of God',
+            'release_year'=>'2003'
+        ],
+        [
+            'title'=>'Godfather',
+            'release_year'=>'1972'
+        ],
+        [
+            'title'=>'Titanic',
+            'release_year'=>'1997'
+        ]
+    ];
+    ?>
+</body>
+</html>
